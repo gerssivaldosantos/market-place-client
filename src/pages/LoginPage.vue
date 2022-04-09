599d802c9cadc0ca68df0f871a9962a4831d00cf
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-04-09 03:03:33 -0300

    feat: create login page

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,7 +1,57 @@
-<script lang="ts" setup>
+<script lang="ts" setup>import { ref } from 'vue'
 
+const email = ref<string>('')
+const password = ref<string>('')
+const submitForm = () => {
+  console.log('form')
+}
 </script>
 
 <template>
-  <q-page></q-page>
+  <img src="../assets/wave.png" class="wave" alt="login-wave" />
+  <div class="row" style="height: 90vh">
+    <div class="col-0 col-md-6 flex justify-center content-center">
+      <!-- <img src="../assets/login.svg" class="responsive" alt="login-image" /> -->
+    </div>
+    <div
+      v-bind:class="{ 'justify-center': $q.screen.md || $q.screen.sm || $q.screen.xs }"
+      class="col-12 col-md-6 flex content-center"
+    >
+      <q-card v-bind:style="$q.screen.lt.sm ? { 'width': '80%' } : { 'width': '50%' }">
+        <q-card-section>
+          <q-avatar size="103px" class="absolute-center shadow-10">
+            <img src="../assets/avatar.png" alt="avatar" />
+          </q-avatar>
+        </q-card-section>
+        <q-card-section>
+          <div class="q-pt-lg">
+            <div class="col text-h6 ellipsis flex justify-center"></div>
+          </div>
+        </q-card-section>
+        <q-card-section>
+          <q-form class="q-gutter-md" @submit.prevent="submitForm">
+            <q-input label="Username" v-model="email"></q-input>
+            <q-input label="Password" type="password" v-model="password"></q-input>
+            <div>
+              <q-btn class="full-width" color="primary" label="Login" type="submit" rounded></q-btn>
+              <div class="text-center q-mt-sm q-gutter-lg">
+                <router-link class="text-white" to="/login">Esqueceu a senha?</router-link>
+                <router-link class="text-white" to="/login">Criar conta</router-link>
+              </div>
+            </div>
+          </q-form>
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.wave {
+  position: fixed;
+  height: 100%;
+  left: 0;
+  bottom: 0;
+  z-index: -1;
+}
+</style>
