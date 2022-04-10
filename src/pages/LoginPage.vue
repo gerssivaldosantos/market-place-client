a842c327327835b055fe732e183a46f5b3edfdf8
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-04-10 14:28:23 -0300

    feat: make a css ajust

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -36,7 +36,7 @@ onMounted(async () => {
     </div>
     <div v-bind:class="{ 'justify-center': $q.screen.md || $q.screen.sm || $q.screen.xs }"
       class="col-12 col-md-6 flex content-center">
-      <q-card v-bind:style="$q.screen.lt.sm ? { 'width': '80%' } : { 'width': '50%' }">
+      <q-card style="padding: 20px" v-bind:style="$q.screen.lt.sm ? { 'width': '80%' } : { 'width': '50%' }">
         <q-card-section>
           <q-avatar size="103px" class="absolute-center shadow-10">
             <img src="../assets/avatar.png" alt="avatar" />
@@ -52,10 +52,13 @@ onMounted(async () => {
             <q-input label="Username" v-model="email"></q-input>
             <q-input label="Password" type="password" v-model="password"></q-input>
             <div>
-              <q-btn class="full-width" color="primary" label="Login" type="submit" rounded></q-btn>
+              <q-btn style="margin-bottom: 10px" class="full-width" color="primary" label="Login" type="submit" rounded>
+              </q-btn>
               <div class="text-center q-mt-sm q-gutter-lg">
-                <router-link class="text-white" to="/login">Esqueceu a senha?</router-link>
-                <router-link class="text-white" to="/login">Criar conta</router-link>
+                <router-link class="text-grey" style="text-decoration: none" to="/rescue-password">Esqueceu a senha?
+                </router-link>
+                <router-link class="text-grey" style="text-decoration: none" to="/create-account">Criar conta
+                </router-link>
               </div>
             </div>
           </q-form>
@@ -65,10 +68,12 @@ onMounted(async () => {
   </div>
 </template>
 
-<style scoped>.wave {
+<style scoped>
+.wave {
   position: fixed;
   height: 100%;
   left: 0;
   bottom: 0;
   z-index: -1;
-}</style>
+}
+</style>
