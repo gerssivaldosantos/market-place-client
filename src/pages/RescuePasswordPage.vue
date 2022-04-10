e622875295fdff48bfc457a59ec3c3cfcaba968e
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-04-10 16:01:46 -0300

    feat: add rescue password page

---
+++ src/pages/RescuePasswordPage.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+
+import { ref } from 'vue'
+
+const email = ref<string>('')
+const submitForm = async () => { console.log('submitForm') }
+
+</script>
+
+<template>
+  <q-card style="padding: 20px" v-bind:style="$q.screen.lt.sm ? { 'width': '80%' } : { 'width': '50%' }">
+    <q-card-section>
+      <q-form class="q-gutter-md" @submit.prevent="submitForm">
+        <q-input label="email" v-model="email"></q-input>
+        <div>
+          <q-btn style="margin-bottom: 10px" class="full-width" color="primary" label="Send Link" type="submit" rounded>
+          </q-btn>
+          <div class="text-center q-mt-sm q-gutter-lg">
+            <router-link class="text-grey" style="text-decoration: none" to="/login">
+              <q-btn flat icon="vpn_key"> Login </q-btn>
+            </router-link>
+          </div>
+        </div>
+      </q-form>
+    </q-card-section></q-card>
+</template>
