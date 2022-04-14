31c8a37b64cf0a1971433b73c24eab356a0aeb51
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-04-14 00:02:19 -0300

    feat: add congratulations page

---
+++ src/pages/CongratulationsPage.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <q-card style="padding: 20px" v-bind:style="$q.screen.lt.sm ? { 'width': '80%' } : { 'width': '50%' }">
+    <q-card-section>
+      <h4 class="Headline-1 text-grey-7">Congratulations ! Your email are actived.
+      </h4>
+      <q-btn to="/login/access" icon="key" padding="10px" color="blue">Login</q-btn>
+    </q-card-section>
+  </q-card>
+</template>
