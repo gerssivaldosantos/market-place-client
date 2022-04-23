75300cfc3ff9fcaef0d281cea059fab91aa07f5e
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-04-23 01:30:07 -0300

    feat: add page example

--- src/pages/OrdersPage.vue
+++ src/pages/OrdersPage.vue
@@ -1,36 +1,105 @@
 <template>
   <div class="q-pa-md">
-    <q-table separator="none" :rows="rows" row-key="name" />
-    <br>
-    <q-btn @click="onClickAddProduct" label="Add New" color="blue"></q-btn>
+    <q-table grid title="Treats" :rows="rows" :columns="columns" row-key="name" :filter="filter" hide-header>
+      <template v-slot:top-right>
+        <q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+    </q-table>
   </div>
 </template>
 
-<script lang="ts" setup>import { useQuasar } from 'quasar'
-import CreateProductVue from 'src/components/CreateProduct.vue'
-import { useRequest } from 'src/helpers/useRequest'
-import { ref, onMounted } from 'vue'
+<script>
+import { ref } from 'vue'
 
-const $q = useQuasar()
+const columns = [
+  {
+    name: 'desc',
+    required: true,
+    label: 'Dessert (100g serving)',
+    align: 'left',
+    field: row => row.name,
+    format: val => `${val}`,
+    sortable: true
+  },
+  { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
+  { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+  { name: 'carbs', label: 'Carbs (g)', field: 'carbs' }
+]
 
-const onClickAddProduct = () => {
-  $q.dialog({
-    component: CreateProductVue
-  }).onOk(async () => {
-    const result = await useRequest.get('orders')
-    rows.value = result.data.content
-  }).onCancel(() => {
-    console.log('Cancel')
-  }).onDismiss(() => {
-    console.log('Called on OK or Cancel')
-  })
+const rows = [
+  {
+    name: 'Frozen Yogurt',
+    calories: 159,
+    fat: 6.0,
+    carbs: 24
+  },
+  {
+    name: 'Ice cream sandwich',
+    calories: 237,
+    fat: 9.0,
+    carbs: 37
+  },
+  {
+    name: 'Eclair',
+    calories: 262,
+    fat: 16.0,
+    carbs: 23
+  },
+  {
+    name: 'Cupcake',
+    calories: 305,
+    fat: 3.7,
+    carbs: 67
+  },
+  {
+    name: 'Gingerbread',
+    calories: 356,
+    fat: 16.0,
+    carbs: 49
+  },
+  {
+    name: 'Jelly bean',
+    calories: 375,
+    fat: 0.0,
+    carbs: 94
+  },
+  {
+    name: 'Lollipop',
+    calories: 392,
+    fat: 0.2,
+    carbs: 98
+  },
+  {
+    name: 'Honeycomb',
+    calories: 408,
+    fat: 3.2,
+    carbs: 87
+  },
+  {
+    name: 'Donut',
+    calories: 452,
+    fat: 25.0,
+    carbs: 51
+  },
+  {
+    name: 'KitKat',
+    calories: 518,
+    fat: 26.0,
+    carbs: 65
+  }
+]
+
+export default {
+  setup () {
+    return {
+      filter: ref(''),
+      columns,
+      rows
+    }
+  }
 }
-
-const rows = ref<[]>([])
-
-onMounted(async () => {
-  const result = await useRequest.get('orders')
-  rows.value = result.data.content
-})
-
 </script>
