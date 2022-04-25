a6f039fb5e88e331888cb65f378776c627fb693a
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-04-24 23:25:31 -0300

    feat:  new button for logout and show infos, set dark mode

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,7 +4,29 @@
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
         <q-toolbar-title>Market Place</q-toolbar-title>
-        <q-btn flat icon="logout" @click="onLogout" />
+        <q-btn style="margin-right: 15px;" flat dense round icon="shopping_cart" />
+        <q-btn flat icon="keyboard_arrow_down">
+          <q-menu :offset="[0, 10]">
+            <div class="row no-wrap q-pa-md">
+              <div class="column">
+                <div class="text-h6 q-mb-md">Settings</div>
+                <q-toggle label="Dark Mode" @update:model-value="onDarkModeSelected" v-model="darkMode" />
+              </div>
+
+              <q-separator vertical inset class="q-mx-lg" />
+
+              <div class="column items-center">
+                <q-avatar size="72px">
+                  <img src="../assets/avatar.png">
+                </q-avatar>
+
+                <div class="text-subtitle1 q-mt-md q-mb-xs">John Doe</div>
+
+                <q-btn @click="onLogout" color="primary" label="Logout" push size="sm" v-close-popup />
+              </div>
+            </div>
+          </q-menu>
+        </q-btn>
       </q-toolbar>
     </q-header>
 
@@ -24,21 +46,39 @@
 <script lang="ts" setup>
 import RouteLink from 'components/RouteLink.vue'
 import PreferencesDialog from 'components/PreferencesDialog.vue'
-import { useQuasar } from 'quasar'
+import { LocalStorage, useQuasar } from 'quasar'
 import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
+import { useGeneralStore } from 'src/stores/generalStore'
+
+/* const cart = ref<string[]>([]) */
 
 onMounted(() => {
   if (!localStorage.getItem('token')) { useRouter().push('/login/access') }
 })
+const $s = useGeneralStore()
+const darkMode = ref<boolean>($s.darkMode)
 const router = useRouter()
 const leftDrawerOpen = ref(false)
 const toggleLeftDrawer = () => {
   leftDrawerOpen.value = !leftDrawerOpen.value
 }
+
+const onDarkModeSelected = (value: boolean) => {
+  $q.dark.set(value)
+  LocalStorage.set('darkMode', value)
+}
+
 const onLogout = async () => {
-  localStorage.clear()
-  await router.push('/login/access')
+  $q.dialog({
+    title: 'Disconnect',
+    message: 'Are you sure you want Logout ?',
+    ok: 'Yes',
+    cancel: 'No'
+  }).onOk(async () => {
+    localStorage.clear()
+    await router.push('/login/access')
+  })
 }
 
 const $q = useQuasar()
