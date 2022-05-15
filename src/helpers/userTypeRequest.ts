e33e937a5987a4f95cad2cd2de8e8231e696d72d
gerssivaldosantos <canalandows@gmail.com>
2022-05-15 12:46:57 -0300

    feat: create getAll

---
+++ src/helpers/userTypeRequest.ts
@@ -0,0 +1,10 @@
+import axios, { AxiosResponse } from 'axios'
+import { userTypeDto } from 'src/dtos/userType'
+import { baseUrl, getHeaders } from './useRequest'
+
+export const useUserType = {
+  async getAll (headers = {}):Promise<userTypeDto> {
+    const result:AxiosResponse = await axios.get(`${baseUrl}/user-types`, { headers: getHeaders(headers) })
+    return result.data
+  }
+}
