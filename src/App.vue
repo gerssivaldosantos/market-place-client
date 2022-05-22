5295a2a3e7bbf35e0160b15b634a4aafed269fc2
gerssivaldosantos <canalandows@gmail.com>
2022-05-22 12:18:21 -0300

    feat: add notify errors

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script lang="ts" setup>
-/* import axios from 'axios' */
+import axios from 'axios'
 import { LocalStorage, useQuasar } from 'quasar'
 import { onMounted, watch } from 'vue'
 import { useRouter } from 'vue-router'
@@ -15,7 +15,7 @@ const $s = useGeneralStore()
 const $q = useQuasar()
 const router = useRouter()
 
-/* const triggerMessage = (message: string | null, type: string | null) => {
+const triggerMessage = (message: string | null, type: string | null) => {
   if (!message) {
     message = 'Not Found'
   }
@@ -26,23 +26,16 @@ const router = useRouter()
 }
 
 axios.interceptors.response.use(
-  response => {
-    if (response.status === 200) {
-      triggerMessage(response.data.message, 'blue')
-    }
-    return response
-  },
+  response => response,
   (error) => {
     if (error.response.status === 500) {
       console.log(error.response)
       triggerMessage(error.response.data.message, 'negative')
     } else if (error.response.status === 404 || error.response.status === 403 || error.response.status === 401 || error.response.status === 400) {
       triggerMessage(error.response.data.message, 'warning')
-    } else {
-      triggerMessage(null, 'negative')
     }
   }
-) */
+)
 watch(() => $s.darkMode, (newValue) => {
   $q.dark.set(newValue)
 })
